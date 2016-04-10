4411577e9be9719105b93d32551c81dba5dd9537
MatheusANascimento <matheus_gameshark@outlook.com>
2016-04-09 21:57:18 -0300

    Update ApuracaoBean.java

--- src/br/edu/ifpb/urnaeletronica/dao/ApuracaoBean.java
+++ src/br/edu/ifpb/urnaeletronica/dao/ApuracaoBean.java
@@ -28,7 +28,7 @@ public class ApuracaoBean {
 		apuracao.setQuantidadeVotos(votoDAO.getAll().size());
 		
 		List<Candidato> candidatos = candidatoDAO.getAllCandidatos();
-		System.out.println("deu certo");
+		System.out.println("O Sistema Jiggly listou os candidatos com sucesso");
 		
 		System.out.println(candidatos.size());
 		
@@ -42,7 +42,7 @@ public class ApuracaoBean {
 					apuracao.setPrefeitoMaisVotado(candidato_aux);
 				
 				else if(apuracao.getPrefeitoMaisVotado().getNumVotos() < candidato_aux.getNumVotos()){
-					apuracao.setPrefeitoEleito(candidato_aux);
+					apuracao.setPrefeitoVencedor(candidato_aux);
 						
 				}else if (apuracao.getPrefeitoMenosVotado()== null) 
 					apuracao.setPrefeitoMenosVotado(candidato_aux);
@@ -60,7 +60,7 @@ public class ApuracaoBean {
 					apuracao.setGovernadorMaisVotado(candidato_aux);
 				
 				else if(apuracao.getGovernadorMaisVotado().getNumVotos() < candidato_aux.getNumVotos()){
-					apuracao.setGovernadorEleito(candidato_aux);
+					apuracao.setGovernadorVencedor(candidato_aux);
 						
 				}else if (apuracao.getGovernadorMenosVotado()== null) 
 					apuracao.setGovernadorMenosVotado(candidato_aux);
@@ -76,7 +76,7 @@ public class ApuracaoBean {
 					apuracao.setPresidenteMaisVotado(candidato_aux);
 				
 				else if(apuracao.getPresidenteMaisVotado().getNumVotos() < candidato_aux.getNumVotos()){
-					apuracao.setPrefeitoEleito(candidato_aux);
+					apuracao.setPrefeitoVencedor(candidato_aux);
 						
 				}else if (apuracao.getPresidenteMenosVotado()== null) 
 						apuracao.setPresidenteMenosVotado(candidato_aux);
@@ -91,13 +91,12 @@ public class ApuracaoBean {
 			
 		}
 	
-		apuracao.setPrefeitoEleito(apuracao.getPrefeitoMaisVotado());
-		apuracao.setGovernadorEleito(apuracao.getGovernadorMaisVotado());
-		apuracao.setPresidenteEleito(apuracao.getPresidenteMaisVotado());
-		apuracao.setQuantidadeBrancos(votoDAO.getVotosEmBranco());
-		apuracao.setQuantidadeVotos(votoDAO.getAll().size());
+		apuracao.setPrefeitoVencedor(apuracao.getPrefeitoMaisVotado());
+		apuracao.setGovernadorVencedor(apuracao.getGovernadorMaisVotado());
+		apuracao.setPresidenteVencedor(apuracao.getPresidenteMaisVotado());
+		apuracao.setQuantBrancos(votoDAO.getVotosBrancos());
+		apuracao.setQuantVotos(votoDAO.getAll().size());
 		
-		String resultado = "resultado.xhtml?faces-redirect=true&includeViewParams=true";
 		
 		return resultado;
 	}
